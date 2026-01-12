9e403d20812606f24b68bb18c655930aa8d28502
mensivzh <30454860+Nuzul07@users.noreply.github.com>
2026-01-12 16:15:37 +0700

    tambah nama nuzul

--- app/Http/Controllers/AdminControllers/BagianLainController.php
+++ app/Http/Controllers/AdminControllers/BagianLainController.php
@@ -18,6 +18,7 @@ class BagianLainController extends Controller
     public function SewaModalIndex()
     {
         $haha = "huhu";
+        $nama = "nuzul";
         return Inertia::render('Admin/MasterData/SewaModal');
     }
 
